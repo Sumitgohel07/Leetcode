6a65934ce1e4cd2207e9ff2bb575b7ce2e3da83e
Sumit Gohel <sumitgohel576@gmail.com>
2026-07-22 11:06:21 +0530

    Time: 6 ms (56.81%) | Memory: 22.8 MB (80.24%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -6,21 +6,12 @@ class Solution:
                 my_dict[key] = 1
             else:
                 my_dict[key] += 1
+        sorted_item = sorted(my_dict.items(),key = lambda x : x[1],reverse=True)
+        freq_list = []
+        for key,value in sorted_item:
+            freq_list.append([key,value])
         result = []
         for i in range(k):
-            temp = max(my_dict,key = my_dict.get )
-            result.append(temp)
-            del my_dict[temp]
+            result.append(freq_list[i][0])
         return result
-            
-
-
-        # sorted_item = sorted(my_dict.items(),key = lambda x : x[1],reverse=True)
-        # freq_list = []
-        # for key,value in sorted_item:
-        #     freq_list.append([key,value])
-        # result = []
-        # for i in range(k):
-        #     result.append(freq_list[i][0])
-        # return result
 
