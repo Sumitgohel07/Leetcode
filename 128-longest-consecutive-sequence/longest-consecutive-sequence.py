0420df37330623497e6fe4821bc40bf4900399be
Sumit Gohel <sumitgohel576@gmail.com>
2026-07-25 00:06:28 +0530

    Time: 43 ms (85.29%) | Memory: 36.8 MB (12.92%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -11,14 +11,3 @@ class Solution:
                     current+=1
                 result = count if count>result else result
         return result
-
-        # sort  = set(nums)
-        # count = 1
-        # result = []
-        # for num in sorted(sort):
-        #     if num+1 in sort:
-        #         count+=1
-        #     else:
-        #         result.append(count)
-        #         count = 1
-        # return max(result,default=0)
