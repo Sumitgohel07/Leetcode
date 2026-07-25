f0126705914ac23f98362ecd9cb563cbf6aef1c3
Sumit Gohel <sumitgohel576@gmail.com>
2026-07-25 18:18:47 +0530

    Time: 7 ms (81.17%) | Memory: 19.5 MB (76.43%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,7 +4,7 @@ class Solution:
         for char in s:
             if char.isalnum():
                 s1 += char.lower()
-        for i in range(len(s1)):
+        for i in range(len(s1)//2):
             if s1[i] != s1[len(s1)-1-i]:
                 return False
         return True
