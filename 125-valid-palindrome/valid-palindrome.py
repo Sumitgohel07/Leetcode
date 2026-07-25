70fb1861f56d9a93fe99dd9dede82fbcbc68ea0e
Sumit Gohel <sumitgohel576@gmail.com>
2026-07-25 18:17:27 +0530

    Time: 11 ms (31.87%) | Memory: 19.5 MB (76.43%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s1 = ""
+        for char in s:
+            if char.isalnum():
+                s1 += char.lower()
+        for i in range(len(s1)):
+            if s1[i] != s1[len(s1)-1-i]:
+                return False
+        return True
