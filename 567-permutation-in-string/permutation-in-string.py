e2251aeed0e98f0fca325588aeac1ce9625cf37e
Sumit Gohel <sumitgohel576@gmail.com>
2026-08-08 12:11:02 +0530

    Time: 2114 ms (12.00%) | Memory: 19.3 MB (53.71%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -5,11 +5,9 @@ class Solution:
             s1_count[i] = s1_count.get(i,0)+1
         left = 0
         right = len(s1)
-        window_list = []
         while right <= len(s2):
             temp_count = dict()
             word = s2[left:right]
-            window_list.append(word)
             for i in word:
                 temp_count[i] = temp_count.get(i,0)+1
             if s1_count == temp_count:
