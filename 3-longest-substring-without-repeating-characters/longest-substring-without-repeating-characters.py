fadbafac04c64f02ac5733f675d9b7cc5909c448
Sumit Gohel <sumitgohel576@gmail.com>
2026-08-08 06:13:00 +0530

    Time: 363 ms (5.01%) | Memory: 19.9 MB (13.39%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,7 +4,7 @@ class Solution:
         left=0
         right=0
         seen = set()
-        while left <= right and right < len(s):
+        while right < len(s):
             if s[right] not in seen:
                 seen.add(s[right])
                 right+=1
