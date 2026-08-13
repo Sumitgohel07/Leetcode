95841d8da003a696ccb24816096135ef2a13ece2
Sumit Gohel <sumitgohel576@gmail.com>
2026-08-13 12:00:07 +0530

    Time: 19 ms (60.05%) | Memory: 19.3 MB (86.25%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -7,17 +7,22 @@ class Solution:
 
         left = 0
         right = len(s1)
-
+        temp_count = dict()
+        flag = 1
         while right <= len(s2):
-            temp_count = dict()
-            word = s2[left:right]
-
-            for i in word:
-                temp_count[i] = temp_count.get(i, 0) + 1
+            if flag == 1:
+                for i in range(left,right):
+                    temp_count[s2[i]] = temp_count.get(s2[i], 0) + 1
+                    flag = 0
 
             if s1_count == temp_count:
                 return True
-
+            else:
+                temp_count[s2[left]] -= 1
+                if temp_count[s2[left]] == 0:
+                    del temp_count[s2[left]]
+                if right < len(s2):
+                    temp_count[s2[right]] = temp_count.get(s2[right],0) + 1
             left += 1
             right += 1
 
