0b8abc59310e67879dc4d3b8ef2b493c5dc5f016
Sumit Gohel <sumitgohel576@gmail.com>
2026-08-13 15:22:06 +0530

    Time: 24 ms (15.32%) | Memory: 30.6 MB (41.49%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -3,10 +3,13 @@ class Solution:
         left = 0 
         window_sum = 0
         min_len = len(nums) + 1
+
         for right in range(len(nums)):
             window_sum += nums[right]
+
             while window_sum >= target:
-                min_len = min(min_len,right-left+1)
+                min_len = min(min_len, right - left + 1)
                 window_sum -= nums[left]
-                left += 1        
+                left += 1
+
         return 0 if min_len == len(nums) + 1 else min_len
