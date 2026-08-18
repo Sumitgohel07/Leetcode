c24095dfb1b00d77170a265ccce26d4de6e004b5
Sumit Gohel <sumitgohel576@gmail.com>
2026-08-18 07:16:06 +0530

    Time: 67 ms (60.50%) | Memory: 19.6 MB (94.38%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
+        t_dict = {}
+        for i in range(len(t)):
+            t_dict[t[i]] = t_dict.get(t[i],0) + 1
+        left = 0
+        window_dict = {}
+        need = len(t_dict)
+        have = 0
+        best_left = left
+        best_len = len(s) + 1
+        for right in range(len(s)):
+            if s[right] in t:
+                window_dict[s[right]] = window_dict.get(s[right],0) + 1
+                if t_dict[s[right]] == window_dict[s[right]]:
+                    have+=1
+            while have == need:
+                if right - left + 1 < best_len:
+                    best_len = right - left + 1
+                    best_left = left
+                if s[left] in window_dict:
+                    if window_dict[s[left]] == t_dict[s[left]]:
+                        have -= 1
+
+                    window_dict[s[left]] -= 1
+
+                    if window_dict[s[left]] == 0:
+                        del window_dict[s[left]]
+
+                left += 1
+        return "" if best_len == len(s) + 1 else s[best_left:best_left + best_len]  
